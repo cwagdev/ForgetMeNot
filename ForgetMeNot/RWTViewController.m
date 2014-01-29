76118eb46a53242fe4ebbc2b70efda5a2a71c537
Chris Wagner <cwagner118@gmail.com>
2014-01-28 23:54:55 -0700

    Initial Commit

---
+++ ForgetMeNot/RWTViewController.m
@@ -0,0 +1,29 @@
+//
+//  RWTViewController.m
+//  ForgetMeNot
+//
+//  Created by Chris Wagner on 1/28/14.
+//  Copyright (c) 2014 Ray Wenderlich Tutorial Team. All rights reserved.
+//
+
+#import "RWTViewController.h"
+
+@interface RWTViewController ()
+
+@end
+
+@implementation RWTViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
